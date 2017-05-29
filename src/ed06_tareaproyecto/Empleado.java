8512c3db683e6219fa8ce97c0b14c6d171d97762
CarlosJavier <CarlosJavier@CGARTO>
2017-05-29 18:03:08 +0200

    Tarea de Entornos de Desarrollo github v.1. & Netbeans

---
+++ src/ed06_tareaproyecto/Empleado.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ed06_tareaproyecto;
+
+/**
+ *
+ * @author ED06
+ */
+public class Empleado {
+    private String dni;
+    private String nombre;
+    private String direccion;
+    private String cargo;
+    private int numHijos;
+    
+    public Empleado (String dni, String nombre){
+        this.dni=dni;
+        this.nombre=nombre;
+        this.cargo="EMPLEADO";
+        this.numHijos=0;
+    }
+    
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getCargo() {
+        return cargo;
+    }
+
+    public void setCargo(String cargo) {
+        this.cargo = cargo;
+    }
+
+    public int getNumHijos() {
+        return numHijos;
+    }
+
+    public void setNumHijos(int numHijos) {
+        this.numHijos = numHijos;
+    }
+}
---
+++ src/ed06_tareaproyecto/Main.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ed06_tareaproyecto;
+/**
+ *
+ * @author ED06
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    
+    public static void main(String[] args) {
+        Empleado empleado1 = new Empleado("1245678M","Perico de los Palotes");
+        
+        empleado1.setDireccion("C/del Pez,6");
+        empleado1.setNumHijos(3);
+        imprimir_emp(empleado1);
+    }
+
+    public static void imprimir_emp(Empleado empleado1) {
+        System.out.println("DNI: "+empleado1.getDni());
+        System.out.println("Nombre: "+empleado1.getNombre());
+        System.out.println("Cargo: "+empleado1.getCargo());
+        System.out.println("Número de hijos: "+empleado1.getNumHijos());
+        double total = 1200;
+        if (empleado1.getCargo().equals("ENCARGADO")) {
+            total += 300;
+        } else if (empleado1.getCargo().equals("DIRECTOR")) {
+            total += 1000;
+        }
+        if (empleado1.getNumHijos() >= 3) {
+            total += empleado1.getNumHijos() * 50;
+        }
+        System.out.println("Sueldo: "+total);
+    }
+    
+    
+}
